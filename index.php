28573850e87efcd9129c8543392c5de75e17917e
Brendan Shea <bjrshea@gmail.com>
2019-01-17 13:37:06 -0800

    Sanitize code with htmlspecialchars func.

--- index.php
+++ index.php
@@ -1,5 +1,15 @@
-<?php
-
-$name = 'Brendan';
-
-echo 'Hello, ' . $name;
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>PHP Practice</title>
+    <link rel="stylesheet" href="styles.css">
+  </head>
+  <body>
+    <header>
+      <h1>
+        <?= "Hello, " . htmlspecialchars($_GET['name']); ?>
+      </h1>
+    </header>
+  </body>
+</html>
